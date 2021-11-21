d83b71b9b06a2e394bc5e344751e2c9f145768e0
vaio.root <68550165+vaio-php@users.noreply.github.com>
2021-11-21 17:18:10 +0400

    Add files via upload

---
+++ Ddos-Attack-Aze.py
@@ -0,0 +1,10 @@
+print("""
+[H]=========HARMAN========[-][o][x]
+|                                 |
+|     Programa Hoşgeldiniz!       |
+|           Sürüm 0.8             |
+|    Devam etmek için herhangi    |
+|       bir düğmeye basın.        |
+|                                 |
+|=================================|
+""")
